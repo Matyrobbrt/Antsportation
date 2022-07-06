abf9ba9d6def0adf553a2f9d6524ac7d9c2885f7
Matyrobbrt <matyrobbrt@gmail.com>
2022-07-06 21:03:13 +0300

    Remove unused classes

--- src/main/java/com/matyrobbrt/antsportation/util/RedstoneControl.java
+++ src/main/java/com/matyrobbrt/antsportation/util/RedstoneControl.java
@@ -1,14 +1,10 @@
 package com.matyrobbrt.antsportation.util;
 
-import com.matyrobbrt.antsportation.Antsportation;
-import com.matyrobbrt.antsportation.block.entity.BlockEntityProperty;
 import net.minecraft.network.chat.Component;
 
 public enum RedstoneControl {
     DISABLED(Translations.REDSTONE_DISABLED), HIGH(Translations.REDSTONE_HIGH),
     LOW(Translations.REDSTONE_LOW), PULSE(Translations.REDSTONE_PULSE);
-
-    public static final BlockEntityProperty<RedstoneControl> PROPERTY = BlockEntityProperty.enumProperty(Antsportation.rl("redstone_control"), RedstoneControl.class);
 
     private final Translations translation;
 
