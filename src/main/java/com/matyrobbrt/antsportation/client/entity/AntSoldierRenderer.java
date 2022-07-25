ee2a2217dadae58d8de85c6a31b455ef267a5c89
Learwin <6223515+Learwin@users.noreply.github.com>
2022-07-25 18:31:49 +0200

    Ant Soldier now has it's proper model and animations.

--- src/main/java/com/matyrobbrt/antsportation/client/entity/AntSoldierRenderer.java
+++ src/main/java/com/matyrobbrt/antsportation/client/entity/AntSoldierRenderer.java
@@ -7,7 +7,7 @@ import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
 public class AntSoldierRenderer extends MobRenderer<AntSoldierEntity, AntSoldierModel<AntSoldierEntity>> {
-    public static final ResourceLocation ANT_SOLDIER_LOCATION = new ResourceLocation(Antsportation.MOD_ID, "textures/entity/ant_queen.png");
+    public static final ResourceLocation ANT_SOLDIER_LOCATION = new ResourceLocation(Antsportation.MOD_ID, "textures/entity/ant_soldier.png");
 
     public AntSoldierRenderer(EntityRendererProvider.Context pContext) {
         super(pContext, new AntSoldierModel<>(pContext.bakeLayer(AntSoldierModel.LAYER_LOCATION)), 0.4F);
