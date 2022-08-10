bc58dbf7009c47d7fd321a6129c07de6c2cc3c0d
Matyrobbrt <matyrobbrt@gmail.com>
2022-08-10 17:19:25 +0300

    Biome modifiers

--- src/data/groovy/com/matyrobbrt/antsportation/data/AntsportationData.groovy
+++ src/data/groovy/com/matyrobbrt/antsportation/data/AntsportationData.groovy
@@ -7,13 +7,19 @@ import com.matyrobbrt.antsportation.data.client.Models
 import com.matyrobbrt.antsportation.data.client.Sounds
 import com.matyrobbrt.antsportation.data.loot.LootProvider
 import com.matyrobbrt.antsportation.data.patchouli.PatchouliProvider
+import com.matyrobbrt.antsportation.data.server.BiomeModifiers
 import com.matyrobbrt.antsportation.data.server.OneTimeJoins
 import com.matyrobbrt.antsportation.data.server.Recipes
 import com.matyrobbrt.antsportation.data.server.Tags
+import com.mojang.serialization.JsonOps
 import groovy.transform.CompileStatic
+import net.minecraft.core.RegistryAccess
+import net.minecraft.resources.RegistryOps
+import net.minecraftforge.common.data.JsonCodecProvider
+import net.minecraftforge.data.event.GatherDataEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
-import net.minecraftforge.forge.event.lifecycle.GatherDataEvent
+import net.minecraftforge.registries.ForgeRegistries
 
 @CompileStatic
 @SuppressWarnings('unused')
@@ -24,31 +30,37 @@ class AntsportationData {
         final var gen = event.generator
         final var existingFileHelper = event.existingFileHelper
 
-        if (event.includeClient()) {
-            final var lang = new Lang(gen)
-            gen.addProvider lang
-            gen.addProvider lang.@enUd
+        // Client
+        final var lang = new Lang(gen)
+        gen.addProvider event.includeClient(), lang
+        gen.addProvider event.includeClient(), lang.@enUd
 
-            gen.addProvider new Models(gen, existingFileHelper)
-            gen.addProvider new Sounds(gen, existingFileHelper)
+        gen.addProvider event.includeClient(), new Models(gen, existingFileHelper)
+        gen.addProvider event.includeClient(), new Sounds(gen, existingFileHelper)
 
-            final var patchouli = new PatchouliProvider(gen)
-            gen.addProvider patchouli
-            gen.addProvider patchouli.@enUd
-        }
-        if (event.includeServer()) {
-            final var advancements = new AdvancementGenerator(gen, existingFileHelper)
+        final var patchouli = new PatchouliProvider(gen)
+        gen.addProvider event.includeClient(), patchouli
+        gen.addProvider event.includeClient(), patchouli.@enUd
 
-            gen.addProvider new Recipes(gen)
-            gen.addProvider(advancements)
-            gen.addProvider new LootProvider(gen, advancements)
 
-            final var blocks = new Tags.Blocks(gen, existingFileHelper)
-            gen.addProvider new Tags.Items(gen, blocks, existingFileHelper)
-            gen.addProvider new Tags.Entities(gen, existingFileHelper)
-            gen.addProvider blocks
+        // Server
+        final var advancements = new AdvancementGenerator(gen, existingFileHelper)
 
-            gen.addProvider new OneTimeJoins(gen, existingFileHelper)
-        }
+        gen.addProvider event.includeServer(), new Recipes(gen)
+        gen.addProvider event.includeServer(), advancements
+        gen.addProvider event.includeServer(), new LootProvider(gen, advancements)
+
+        final var blocks = new Tags.Blocks(gen, existingFileHelper)
+        gen.addProvider event.includeServer(), new Tags.Items(gen, blocks, existingFileHelper)
+        gen.addProvider event.includeServer(), new Tags.Entities(gen, existingFileHelper)
+        gen.addProvider event.includeServer(), blocks
+
+        gen.addProvider event.includeServer(), new OneTimeJoins(gen, existingFileHelper)
+
+        final ops = RegistryOps.create(JsonOps.INSTANCE, RegistryAccess.builtinCopy());
+        gen.addProvider event.includeServer(), JsonCodecProvider.forDatapackRegistry(
+                gen, existingFileHelper, Antsportation.MOD_ID, ops,
+                ForgeRegistries.Keys.BIOME_MODIFIERS, BiomeModifiers.asMap(ops)
+        )
     }
 }
---
+++ src/data/groovy/com/matyrobbrt/antsportation/data/server/BiomeModifiers.groovy
@@ -0,0 +1,42 @@
+package com.matyrobbrt.antsportation.data.server
+
+import com.google.gson.JsonElement
+import com.matyrobbrt.antsportation.Antsportation
+import com.matyrobbrt.antsportation.registration.AntsportationEntities
+import com.matyrobbrt.antsportation.registration.AntsportationPlacedFeatures
+import groovy.transform.CompileStatic
+import net.minecraft.core.HolderSet
+import net.minecraft.core.Registry
+import net.minecraft.resources.RegistryOps
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.tags.BiomeTags
+import net.minecraft.world.level.biome.Biome
+import net.minecraft.world.level.biome.MobSpawnSettings.SpawnerData
+import net.minecraft.world.level.levelgen.GenerationStep
+import net.minecraftforge.common.world.BiomeModifier
+import net.minecraftforge.common.world.ForgeBiomeModifiers
+
+import java.util.function.BiConsumer
+
+@CompileStatic
+class BiomeModifiers  {
+    static void accept(RegistryOps<JsonElement> ops, BiConsumer<String, BiomeModifier> consumer) {
+        final forest = new HolderSet.Named<Biome>(ops.registry(Registry.BIOME_REGISTRY).orElseThrow(), BiomeTags.IS_FOREST)
+        consumer.accept('ant_nests', new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                forest,
+                HolderSet.direct(AntsportationPlacedFeatures.ANT_NEST_PLACED.getHolder().orElseThrow()),
+                GenerationStep.Decoration.TOP_LAYER_MODIFICATION
+        ))
+        consumer.accept('ant_queens', new ForgeBiomeModifiers.AddSpawnsBiomeModifier(
+                forest, List.of(
+                new SpawnerData(AntsportationEntities.ANT_SOLDIER.get(), 1400, 1, 4),
+                new SpawnerData(AntsportationEntities.ANT_QUEEN.get(), 800, 1, 1)
+        )))
+    }
+
+    static Map<ResourceLocation, BiomeModifier> asMap(RegistryOps<JsonElement> ops) {
+        final Map<ResourceLocation, BiomeModifier> map = [:]
+        accept(ops, (name, modifier) -> map[Antsportation.rl(name)] = modifier)
+        return map
+    }
+}
