6640f12a369354f5e9f9832eea4af0fff31d70d2
Itskillerluc <lvgraef@zeelandnet.nl>
2022-08-12 21:25:02 +0200

    optimize assets and changed hill texture

--- src/main/java/com/matyrobbrt/antsportation/block/MarkerBlock.java
+++ src/main/java/com/matyrobbrt/antsportation/block/MarkerBlock.java
@@ -32,15 +32,11 @@ import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RenderShape;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.material.Material;
-import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -49,11 +45,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
@@ -74,7 +66,7 @@ public class MarkerBlock extends BaseEntityBlock implements JEIInfoProvider, Has
     private static final VoxelShape EAST_SHAPE = Shapes.box(0.015625, 0.1875, 0.1875, 0.015635, 0.8125, 0.8125);
 
     private static final boolean USE_DYE = false;
-
+    
     public MarkerBlock(Properties properties) {
         super(properties);
         registerDefaultState(getStateDefinition().any()
